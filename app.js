4430d2b28b68e0293f72244e8086e5f194463ecf
Basil <basiltalias92@gmail.com>
2017-12-03 13:36:02 -0500

    Added comments

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var request = require('request');
 
 var app = express();
 
+//This api will give your city weather details based on your ip address.This is not the best solution.
+//Please use this for educational purpose.
+
 app.post('/', function (req, res) {
     //var geoUrl = 
     async.waterfall([
         function (next) {
+            //Using ip-api to get ip address details
             var ipUrl = 'http://ip-api.com/json';
             request(ipUrl, function (error, response, body) {
                 var bodyJson = JSON.parse(body);
@@ -16,6 +20,7 @@ app.post('/', function (req, res) {
             })
         },
         function (lat, lon, next) {
+            //Weather 
             var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&cnt=10&APPID=bd12f9c41a85e844d4a853a020eb62a5';
             request(weatherUrl, function (error, response, body) {
                 res.json(JSON.parse(body));
